2de2d9ec25897e44f96c78b0b176b0eb60fec918
vbachinsky <vaxa-bobo@ukr.net>
2019-11-14 15:51:38 +0200

    add_sorter_and_common_updater

--- dj_pizza/dj_pizzas/forms.py
+++ dj_pizza/dj_pizzas/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 from .models import Topping
+from django import forms
+
+
+ORDER = (('id', 'id'), ('topping_price', 'цена'), ('topping_description', 'по имени'))
 
 
 class CreateObject(forms.ModelForm):
@@ -14,3 +18,11 @@ class EditObject(forms.ModelForm):
 		model = Topping
 		fields = ['topping_description', 'topping_price']
 		labels = {'topping_description': 'описание топинга', 'topping_price': 'цена'}
+
+
+class UpdateObject(forms.Form):
+	price_change = forms.DecimalField(label='Введите ценовую поправку ', max_digits=5, decimal_places=2)
+
+
+class SorterObject(forms.Form):
+	order = forms.ChoiceField(label='Порядок сортировки: ', choices=ORDER)
